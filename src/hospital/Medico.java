ede7dc0582fd39dd4f75915c5a34bbc8f150f161
Userpl022pc19 <Userpl022pc19@SLFRPL022PC19.ensino.atec.local>
2016-11-08 09:01:07 +0000

    Primeiro Commit

--- src/hospital/Medico.java
+++ src/hospital/Medico.java
@@ -14,7 +14,36 @@ public class Medico {
     int anDouturamento;
     String instEnsino;
     String idLicenca;
-            
+
+    public Medico(int anDouturamento, String instEnsino) {
+        this.anDouturamento = anDouturamento;
+        this.instEnsino = instEnsino;
+    }
+
+    public int getAnDouturamento() {
+        return anDouturamento;
+    }
+
+    public void setAnDouturamento(int anDouturamento) {
+        this.anDouturamento = anDouturamento;
+    }
+
+    public String getInstEnsino() {
+        return instEnsino;
+    }
+
+    public void setInstEnsino(String instEnsino) {
+        this.instEnsino = instEnsino;
+    }
+
+    public String getIdLicenca() {
+        return idLicenca;
+    }
+
+    public void setIdLicenca(String idLicenca) {
+        this.idLicenca = idLicenca;
+    }
+    
     public void prescreverTratamento() {
         
     }
